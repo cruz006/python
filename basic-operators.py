f7879892c8e5a51f6d87591763afbc4f02a34397
aacruz01 <100636330+aacruz01@users.noreply.github.com>
2023-04-18 14:59:34 -0700

    chore: finish file

--- basic-operators.py
+++ basic-operators.py
@@ -44,4 +44,3 @@ big_list = x_list + y_list
 print("x_list contains %d objects" % len(x_list))
 print("y_list contains %d objects" % len(y_list))
 print("big_list contains %d objects" % len(big_list))
-

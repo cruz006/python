37ef5ff2749b5095d891d148a2dc2ff78674667f
aacruz01 <100636330+aacruz01@users.noreply.github.com>
2023-04-18 13:13:11 -0700

    strings.py content

--- strings.py
+++ strings.py
@@ -2,3 +2,42 @@
 #   to make a string a string, you need to wrap it in quotes as such:
 message = "this is a string"
 print(message)
+
+#   for qoutes this is how you wouls do it
+single = 'Single quote allow you to embed "double" quotes in your string.'
+double = "Double quote allow you to embed 'single' quotes in your string."
+triple = """Triple quotes allows to embed "double quotes" as well as 'single quotes' in your string. And can also span across multiple lines."""
+print(single + " " + double + " " + triple)
+
+#   this is how you can find the length of a certain string, i will be using the previous strings
+var1 = len(single)
+print(var1)
+
+#   THIS WHOLE SEGMENT WILL USE THE SAME STRING
+
+#    then this is how you can use the index of a string to find a certain letter
+#    using the index, it reads left to right starting at 0, and it goes by letter
+snack = "Chocolate cookie."
+print(snack[0])
+
+#    then this would be to find a whole word from it using the index
+print(snack[10:16])
+
+#    these are ways that you could use index to print out the whole message
+#   Stop value not provided, goes on forever
+print(snack[0:]) 
+
+#   Start value not provided (Stop value excluded according to syntax)
+print(snack[:-1])
+
+#   This is also allowed
+print(snack[:])
+
+#   then this is how you could print a list in reverse
+num_string = "12345678910"
+print(num_string[::-1]) 
+
+#   this is how you could make it print every other one, the first digit 
+#   will be the starting, then it is the order, and then the second order
+print(num_string[0:-1:2])
+

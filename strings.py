f57da697efc5d2f8ed482737a0686ca30ee05d11
aacruz01 <100636330+aacruz01@users.noreply.github.com>
2023-04-17 19:50:00 -0700

    chore: make strings

---
+++ strings.py
@@ -0,0 +1,4 @@
+#   a string is a data structure where you store a collection of alphabets, words, or characters
+#   to make a string a string, you need to wrap it in quotes as such:
+message = "this is a string"
+print(message)

a49c1e10c6e187a9311a796bd10ea8a37b108d28
aacruz01 <100636330+aacruz01@users.noreply.github.com>
2023-05-10 13:52:57 -0700

    make hangman progress

--- projects/hangman.py
+++ projects/hangman.py
@@ -1,6 +1,7 @@
 #   this will be the code needed for the hangman game code
 #   this is where we get the name of the user
-name = "Hello, what is your name?"
-print(name)
+print('Hello, what is your name?')
+my_name = input()
 
-#   now you need to get the input 
+#   now you greet them
+print('Hello, {}'.format (my_name) + ' would you like to play hangman?')

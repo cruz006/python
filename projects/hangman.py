391fa05bc4d17fe86b1214cfd32f3f99e2f1238d
aacruz01 <100636330+aacruz01@users.noreply.github.com>
2023-05-11 09:48:03 -0700

    hangman conditonal progress

--- projects/hangman.py
+++ projects/hangman.py
@@ -9,4 +9,18 @@ import time
 time.sleep(3)
 print("Start guessing!")
 
-#   this is where you will have the secret word
+#   this is where you will have the secret word and whatever you guess
+#   you may use whatever word you want, i am using "secret"
+word = "secret"
+guess = ""
+
+#   this determines the number of turns available, you may change this
+#   to whatever you want
+turns = 10
+while turns>0:
+    failed = 0
+
+    #   for every character in word
+    for char in word:
+        if char in guess:
+            print(char, end="")

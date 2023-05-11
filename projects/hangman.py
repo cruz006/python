0e225a9539b648086026f05ffe7493ee86d30d68
aacruz01 <100636330+aacruz01@users.noreply.github.com>
2023-05-11 09:43:50 -0700

    hangman progress

--- projects/hangman.py
+++ projects/hangman.py
@@ -1,7 +1,13 @@
-#   this will be the code needed for the hangman game code
 #   this is where we get the name of the user
 name = input("What is your name? ")
 
 #   now you greet them
-
 print ("Hello, " + name, "Time to play hangman!")
+
+#   wait time
+import time
+time.sleep(3)
+print("Start guessing!")
+
+
+#   this is where you will have the secret word

1b4ad1478eef17e8e80b1da4a2e4976b4a9ffb84
aacruz01 <100636330+aacruz01@users.noreply.github.com>
2023-05-11 09:33:49 -0700

    chore: hangman progress

--- projects/hangman.py
+++ projects/hangman.py
@@ -6,13 +6,13 @@ my_name = input()
 #   now you greet them
 print('Hello, {}'.format (my_name) + ' would you like to play hangman? Please enter "yes" or "no" to continue')
 
-game_on = 'yes'
+game_on = "yes"
 if input() == game_on:
     var1 = "very well"
     print(var1)
 
 
-game_off = 'no'
+game_off = "no"
 if input() == game_off:
     var2 = "dam, ok. you suck"
     print(var2)

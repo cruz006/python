b0117482bff7fca89c3f440df1b5886333cf35fd
aacruz01 <100636330+aacruz01@users.noreply.github.com>
2023-05-11 09:39:58 -0700

    chore: hangman progress

--- projects/hangman.py
+++ projects/hangman.py
@@ -1,16 +1,7 @@
 #   this will be the code needed for the hangman game code
 #   this is where we get the name of the user
 name = input("What is your name? ")
+
 #   now you greet them
 
-
-game_on = "yes"
-if input() == game_on:
-    var1 = "very well"
-    print(var1)
-
-
-game_off = "no"
-if input() == game_off:
-    var2 = "dam, ok. you suck"
-    print(var2)
+print ("Hello, " + name, "Time to play hangman!")

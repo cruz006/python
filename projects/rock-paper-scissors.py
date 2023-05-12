b8de3b15397bf3551228de6341c5c568c0660816
aacruz01 <100636330+aacruz01@users.noreply.github.com>
2023-05-12 09:59:40 -0700

    finish rock-paper-scissors.py

--- projects/rock-paper-scissors.py
+++ projects/rock-paper-scissors.py
@@ -1,2 +1,28 @@
 import random
-action=input("Enter a choice (rock, paper, scissors): ")
+
+action=input("Enter a choice (rock, paper, scissors): \n")
+
+choices = ["rock", "paper", "scissors"]
+computer_choice = random.choice(choices)
+print(f"\nYou chose {action}, computer chose {computer_choice}.\n")
+
+if action == computer_choice:
+    print(f"Both players selected {action}. It's a tie!")
+elif action == "rock":
+
+    if computer_choice == "scissors":
+        print("Rock smashes scissors! You win!")
+    else:
+        print("Paper covers rock! You lose.")
+
+elif action == "paper":
+    if computer_choice == "rock":
+        print("Paper covers rock! You win!")
+    else:
+        print("Scissors cuts paper! You lose.")
+
+elif action == "scissors":
+    if computer_choice == "paper":
+        print("Scissors cuts paper! You win!")
+    else:
+        print("Rock smashes scissors! You lose.")

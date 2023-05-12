6e09cb42ee06141a338cc10c0d0d8eecfa9ed8fb
aacruz01 <100636330+aacruz01@users.noreply.github.com>
2023-05-12 09:46:45 -0700

    edit code for efficiency

--- basics/commands.py
+++ basics/commands.py
@@ -11,7 +11,7 @@ while var1 < 10:
 #   this FOR loop is similar
 var2 = ["A", "B", "C", "D"]
 for  a in var2:
-    print(a)
+    print(a + "\n")
     
 
 #   this for loop prints all numbers between 1 and 10. 
@@ -21,7 +21,7 @@ for i in range(1,10):
 
 #   this is a type command, which is used to check the type of an object in your code. 
 #   when you print it, it will show you which class the object is
-my_str = "Why hello there"
+my_str = "Why hello there\n"
 print(type(my_str))
 
 my_int = 5
@@ -36,18 +36,18 @@ print(type(rnum))
 
 #   this is the input command, where it takes your input
 #   then, it prints your input
-var3 = input("Hello, your name is: ")
-print("Hello there " + var3)
+var3 = input("Hello, your name is: \n")
+print("Hello there " + var3) 
 
-var4 = input("Pick any number: ")
-print("The number you said is " + var4 + ", correct?")
+var4 = input("Pick any number: \n")
+print("The number you said is " + var4 + ", correct?\n")
 
 
 #   the len command checks the length of anything
 #   the str command here is used to make the length(an interger),
 #   into a string to be able to print it
-var5 = input("Type anything you want here: ")
-print("What you typed is " + str(len(var5)) + " characters long.")
+var5 = input("Type anything you want here: \n")
+print("What you typed is " + str(len(var5)) + " characters long.\n")
 
 
 #   the str string can be used for many uses

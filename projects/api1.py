d1dc6eff431028e90887823bb0b0fdd23ac7ea47
aacruz01 <100636330+aacruz01@users.noreply.github.com>
2023-05-15 21:45:05 -0700

    save for now, will return

---
+++ projects/api1.py
@@ -0,0 +1,13 @@
+import requests
+
+params = {"words": 10, "paragraphs": 1, "format": "json"}
+
+url = "https://api.coincap.io/v2/assets/"
+
+headers={
+   "X-RapidAPI-Key": "3fc228024fmsh1cf13948472401ep1b25f3jsn88aa0d6e5e7d",
+	"X-RapidAPI-Host": "bit5.p.rapidapi.com"
+ }
+
+response = requests.get(url, headers=headers)
+print(response.json())

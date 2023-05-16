c0a06b314ab6450dee47c90bc8f7fe1ecaecf84d
aacruz01 <100636330+aacruz01@users.noreply.github.com>
2023-05-16 13:14:16 -0700

    bad file, had to remove

--- projects/api1.py
+++ projects/api1.py
@@ -1,11 +1,18 @@
 import requests
 
-url = "https://api.coincap.io/v2/assets/"
+url = "https://openai80.p.rapidapi.com/images/generations"
 
-headers={
-   "X-RapidAPI-Key": "3fc228024fmsh1cf13948472401ep1b25f3jsn88aa0d6e5e7d",
-	"X-RapidAPI-Host": "bit5.p.rapidapi.com"
- }
+payload = {
+	"prompt": input("Please enter a prompt \n"),
+	"n": 2,
+	"size": "1024x1024"
+}
 
-response = requests.get(url, headers=headers)
+headers = {
+	"content-type": "application/json",
+	"X-RapidAPI-Key": "3fc228024fmsh1cf13948472401ep1b25f3jsn88aa0d6e5e7d",
+	"X-RapidAPI-Host": "openai80.p.rapidapi.com"
+}
+
+response = requests.post(url, json=payload, headers=headers)
 print(response.json())

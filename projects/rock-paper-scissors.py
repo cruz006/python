2e16e819d6defd59252ace455b036f2f25699c50
aacruz01 <100636330+aacruz01@users.noreply.github.com>
2023-06-30 21:24:00 -0700

    organize part 3

--- projects/rock-paper-scissors.py
+++ projects/rock-paper-scissors.py
@@ -1,11 +1,14 @@
 import random
 
+#   gives you the start
 action = input("Enter a choice (rock, paper, scissors): \n")
 
+#   gives the choices
 choices = ["rock", "paper", "scissors"]
 computer_choice = random.choice(choices)
 print(f"\nYou chose {action}, computer chose {computer_choice}.\n")
 
+#   tales the choices and then determines the winner
 if action == computer_choice:
     print(f"Both players selected {action}. It's a tie!")
 elif action == "rock":

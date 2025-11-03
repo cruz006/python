044206b3e593bcb42c4b6bfdf444b778aed6b299
cruz006 <vans5364@gmail.com>
2025-11-03 14:03:30 -0800

    updated version

--- projects/hangman.py
+++ projects/hangman.py
@@ -3,8 +3,61 @@ import random
 def generate_random_word():
     """Generates a random English word from a list."""
 
-    words = ["apple", "banana", "cherry", "dragonfruit", "elderberry",  # Add more words here!
-             "fig", "grape", "honeydew", "jackfruit", "kiwi"]
+    words = [
+    # Fruits
+    "apple", "banana", "cherry", "dragonfruit", "elderberry", "fig", "grape", "honeydew",
+    "jackfruit", "kiwi", "lemon", "mango", "nectarine", "orange", "papaya", "quince",
+    "raspberry", "strawberry", "tangerine", "ugli", "vanilla", "watermelon", "xigua",
+    "yellowplum", "zucchini", "apricot", "blackberry", "blueberry", "cranberry",
+    "date", "gooseberry", "grapefruit", "lychee", "mulberry", "olive", "passionfruit",
+    "pear", "persimmon", "pineapple", "plum", "pomegranate", "redcurrant", "soursop",
+    "starfruit", "tomato", "avocado", "coconut", "durian", "guava", "rambutan",
+
+    # Animals
+    "aardvark", "antelope", "bear", "buffalo", "cat", "cheetah", "chicken", "chimpanzee",
+    "cow", "coyote", "crab", "deer", "dog", "dolphin", "duck", "eagle", "elephant", "falcon",
+    "ferret", "flamingo", "fox", "frog", "giraffe", "goat", "gorilla", "hamster", "hedgehog",
+    "hippopotamus", "horse", "jaguar", "jellyfish", "kangaroo", "koala", "lemur", "leopard",
+    "lion", "lizard", "lobster", "monkey", "moose", "octopus", "ostrich", "otter", "owl",
+    "panda", "parrot", "peacock", "penguin", "pig", "porcupine", "rabbit", "raccoon",
+    "rat", "reindeer", "rhino", "seal", "shark", "sheep", "sloth", "snail", "snake",
+    "sparrow", "spider", "squid", "squirrel", "tiger", "turtle", "vulture", "walrus",
+    "weasel", "whale", "wolf", "yak", "zebra",
+
+    # Colors
+    "red", "orange", "yellow", "green", "blue", "indigo", "violet", "white", "black",
+    "gray", "pink", "purple", "brown", "teal", "turquoise", "magenta", "gold", "silver",
+    "bronze", "beige", "maroon", "navy", "olive", "coral", "mint", "lavender",
+
+    # Objects
+    "table", "chair", "desk", "lamp", "window", "door", "carpet", "mirror", "clock",
+    "bottle", "cup", "plate", "spoon", "fork", "knife", "pencil", "book", "notebook",
+    "computer", "keyboard", "mouse", "monitor", "phone", "camera", "headphones", "microphone",
+    "television", "remote", "backpack", "wallet", "watch", "key", "guitar", "violin",
+    "drum", "piano", "bicycle", "skateboard", "helmet", "umbrella", "blanket", "candle",
+    "suitcase", "toothbrush", "comb", "soap", "towel", "pillow", "bed", "couch", "fan",
+    "fridge", "oven", "microwave", "toaster", "blender", "printer", "router", "pen", "eraser",
+
+    # Nature / Miscellaneous
+    "mountain", "river", "forest", "tree", "flower", "cloud", "rain", "snow", "storm",
+    "wind", "sun", "moon", "star", "planet", "galaxy", "ocean", "beach", "island", "valley",
+    "hill", "volcano", "earthquake", "fire", "sand", "rock", "grass", "leaf", "mushroom",
+    "stone", "breeze", "dew", "fog", "ice", "thunder", "tornado",
+
+    # Random concepts / adjectives
+    "happy", "sad", "angry", "calm", "brave", "curious", "clever", "lazy", "kind", "bold",
+    "shy", "strong", "weak", "fast", "slow", "quiet", "loud", "funny", "serious", "smart",
+    "creative", "logical", "emotional", "optimistic", "pessimistic", "confident", "humble",
+    "patient", "wild", "free", "energetic", "tired", "gentle", "chaotic", "focused",
+
+    # Verbs
+    "run", "walk", "jump", "swim", "fly", "sing", "dance", "sleep", "eat", "drink",
+    "read", "write", "draw", "paint", "talk", "listen", "think", "build", "create",
+    "learn", "teach", "code", "play", "laugh", "cry", "smile", "bake", "drive", "ride",
+    "throw", "catch", "push", "pull", "carry", "lift", "cut", "open", "close", "clean",
+    "wash", "fix", "break", "help", "save", "explore", "travel", "discover", "invent"
+]
+
 
     random_index = random.randint(0, len(words) - 1)
     random_word = words[random_index]
